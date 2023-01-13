0b015c5aea5caf9ea02be73cc9b7d369ff625229
Ian Szalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-13 01:10:21 -0800

    added drivetrain and swerve constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,15 +4,56 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+
 import edu.wpi.first.math.util.Units;
 
 public final class Constants {
 
-  // Drivetrain
+  // Drivetrain (no subclass)
 
   // note: these were physically measured center to center of the wheel on a
-  // 29"x29" drivetrain with MK4i's
+  // 29"x29" drivetrain with MK4i's. Ideally these are
   public static final double TRACK_WIDTH = Units.inchesToMeters(23.75);
   public static final double WHEELBASE = Units.inchesToMeters(23.75);
+
+  public static final boolean GYRO_INVERT = false;
+
+  // Swerve Modules
+
+  /*
+   * Order of modules:
+   * 
+   * 0: Front Left
+   * 1: Front Right
+   * 2: Back Left
+   * 3: Back Right
+   * 
+   * 0 1
+   * 2 3
+   *
+   * Like reading
+   * four words
+   */
+
+  // tread is 11 inches long
+  // 11 / 3.14 = 3.50 inch wheel (w/o tread) diameter
+  // tread is 0.3 inches thick
+  // 3.5 + 0.3 = 3.8 inch wheel (w/ tread) diameter
+  private static final double WHEEL_DIAMETER = Units.inchesToMeters(3.8);
+  public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+
+  // https://www.swervedrivespecialties.com/products/mk4i-swerve-module
+  // L2 gearing, Falcon drive motor
+  public static final double DRIVE_GEAR_RATIO = 6.75;
+  public static final double STEER_GEAR_RATIO = 150.0 / 7.0;
+  public static final double MAX_MODULE_SPEED = Units.feetToMeters(16.3);
+
+  public static final boolean DRIVE_MOTOR_INVERT = true;
+  public static final boolean STEER_MOTOR_INVERT = false;
+  public static final boolean ABSOLUTE_MOTOR_INVERT = false;
+
+  public static final NeutralMode DRIVE_NEUTRAL_MODE = NeutralMode.Brake;
+  public static final NeutralMode STEER_NEUTRAL_MODE = NeutralMode.Coast;
 
 }
