e3df174f582f0144645160b291d600ccb3260cc2
Ian Szalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-13 02:01:58 -0800

    added pid values for drive and steer motors

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -1,7 +1,19 @@
 package frc.robot;
 
+import com.frcteam3255.preferences.SN_DoublePreference;
+
 public class RobotPreferences {
   public static final class prefDrivetrain {
+
+    public static final SN_DoublePreference driveF = new SN_DoublePreference("driveF", 0.045);
+    public static final SN_DoublePreference driveP = new SN_DoublePreference("driveP", 0.1);
+    public static final SN_DoublePreference driveI = new SN_DoublePreference("driveI", 0.0);
+    public static final SN_DoublePreference driveD = new SN_DoublePreference("driveD", 1.0);
+
+    public static final SN_DoublePreference steerP = new SN_DoublePreference("steerP", 0.6);
+    public static final SN_DoublePreference steerI = new SN_DoublePreference("steerI", 0.0);
+    public static final SN_DoublePreference steerD = new SN_DoublePreference("steerD", 6.0);
+
   }
 
   public static final class prefVision {
