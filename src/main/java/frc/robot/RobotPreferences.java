facb9cd7924c026b2bbb039b4b9f587d19ad424f
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-14 15:27:46 -0800

    Swerve classes and drivetrain methods (#18)
    * added SN_SwerveModuleConstants
    
    * fix typo
    
    * renamed steerEncoder to absoluteEncoder
    
    * add skeleton of SN_SwerveModule
    
    * added track width and wheelbase to constants
    
    * indentation
    
    * added drivetrain and swerve constants
    
    * added values to mapDrivetrain
    
    * added SN_SwerveModuleConstants for each module
    
    * removed "module" from variable names in SN_SMC
    
    * added pid values for drive and steer motors
    
    * implemented SN_SwerveModule.configure()
    
    * implemented SN_SwerveModule.getState()
    
    * implemented SN_SwerveModule.getAbsoluteEncoder()
    
    * remove unneccesary (and typo) constant
    
    * implemented resetSteerMotorEncoderToAbsolute()
    
    * implemented SN_SwerveModule.setDesiredState()
    
    * added and clarified comments
    
    * added NavX vendor dep
    
    * added start of drivetrain subsystem
    
    * fix typo
    
    * added getPosition() to SN_SwerveModule
    
    * added isDriveOpenLoop method
    
    * added required drivetrain methods
    
    * removed unused constant
    
    * removed todo comment (addressed in an issue)

--- src/main/java/frc/robot/RobotPreferences.java
+++ src/main/java/frc/robot/RobotPreferences.java
@@ -1,7 +1,26 @@
 package frc.robot;
 
+import com.frcteam3255.preferences.SN_BooleanPreference;
+import com.frcteam3255.preferences.SN_DoublePreference;
+
 public class RobotPreferences {
   public static final class prefDrivetrain {
+
+    public static final SN_DoublePreference driveF = new SN_DoublePreference("driveF", 0.045);
+    public static final SN_DoublePreference driveP = new SN_DoublePreference("driveP", 0.1);
+    public static final SN_DoublePreference driveI = new SN_DoublePreference("driveI", 0.0);
+    public static final SN_DoublePreference driveD = new SN_DoublePreference("driveD", 1.0);
+
+    public static final SN_DoublePreference steerP = new SN_DoublePreference("steerP", 0.6);
+    public static final SN_DoublePreference steerI = new SN_DoublePreference("steerI", 0.0);
+    public static final SN_DoublePreference steerD = new SN_DoublePreference("steerD", 6.0);
+
+    // percent of max module speed that is required for the module to steer
+    // (a stopped wheel shouldn't steer)
+    public static final SN_DoublePreference percentToSteer = new SN_DoublePreference("percentToSteer", 0.01);
+
+    public static final SN_BooleanPreference isDriveOpenLoop = new SN_BooleanPreference("isDriveOpenLoop", false);
+
   }
 
   public static final class prefVision {
---
+++ src/main/java/frc/robot/SN_SwerveModule.java
@@ -0,0 +1,239 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.frcteam3255.components.motors.SN_TalonFX;
+import com.frcteam3255.utils.CTREModuleState;
+import com.frcteam3255.utils.SN_Math;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import frc.robot.RobotPreferences.prefDrivetrain;
+
+/**
+ * Coaxial swerve module with SN_TalonFX's for both driving and steering. Uses
+ * CANCoder's for the absolute steering encoders.
+ */
+public class SN_SwerveModule {
+
+  public int moduleNumber;
+
+  private SN_TalonFX driveMotor;
+  private SN_TalonFX steerMotor;
+
+  private CANCoder absoluteEncoder;
+  private double absoluteEncoderOffset;
+
+  private TalonFXConfiguration driveConfiguration;
+  private TalonFXConfiguration steerConfiguration;
+
+  private double lastAngle;
+
+  /**
+   * Create a new SN_SwerveModule.
+   * 
+   * @param moduleConstants Constants required to create a swerve module
+   */
+  public SN_SwerveModule(SN_SwerveModuleConstants moduleConstants) {
+    moduleNumber = moduleConstants.number;
+
+    driveMotor = new SN_TalonFX(moduleConstants.driveMotorID);
+    steerMotor = new SN_TalonFX(moduleConstants.steerMotorID);
+
+    absoluteEncoder = new CANCoder(moduleConstants.absoluteEncoderID);
+    absoluteEncoderOffset = moduleConstants.absoluteEncoderOffset;
+
+    driveConfiguration = new TalonFXConfiguration();
+    steerConfiguration = new TalonFXConfiguration();
+
+    lastAngle = 0;
+
+    configure();
+  }
+
+  /**
+   * Configure the drive motor, steer motor, and absolute encoder.
+   */
+  public void configure() {
+    // driveMotor
+
+    driveMotor.configFactoryDefault();
+
+    driveConfiguration.slot0.kF = prefDrivetrain.driveF.getValue();
+    driveConfiguration.slot0.kP = prefDrivetrain.driveP.getValue();
+    driveConfiguration.slot0.kI = prefDrivetrain.driveI.getValue();
+    driveConfiguration.slot0.kD = prefDrivetrain.driveD.getValue();
+
+    driveMotor.configAllSettings(driveConfiguration);
+
+    driveMotor.setNeutralMode(Constants.DRIVE_NEUTRAL_MODE);
+    driveMotor.setInverted(Constants.DRIVE_MOTOR_INVERT);
+
+    // steerMotor
+
+    steerMotor.configFactoryDefault();
+
+    steerConfiguration.slot0.kP = prefDrivetrain.steerP.getValue();
+    steerConfiguration.slot0.kI = prefDrivetrain.steerI.getValue();
+    steerConfiguration.slot0.kD = prefDrivetrain.steerD.getValue();
+
+    steerMotor.configAllSettings(steerConfiguration);
+
+    steerMotor.setNeutralMode(Constants.STEER_NEUTRAL_MODE);
+    steerMotor.setInverted(Constants.STEER_MOTOR_INVERT);
+
+    // absoluteEncoder
+
+    absoluteEncoder.configFactoryDefault();
+    absoluteEncoder.setPositionToAbsolute();
+  }
+
+  /**
+   * Set the desired state of the swerve module. The state includes the velocity
+   * and angles of the module. The state that the motors are actually set to will
+   * not always be the desired state, rather it will optimized to reduce
+   * unnecessary steering in favor of reversing the direction of the drive motor.
+   * 
+   * @param desiredState    Desired velocity and angle of the module
+   * @param isDriveOpenLoop Is the drive motor velocity set using open or closed
+   *                        loop control
+   */
+  public void setDesiredState(SwerveModuleState desiredState, boolean isDriveOpenLoop) {
+    SwerveModuleState state = CTREModuleState.optimize(desiredState, getState().angle);
+
+    if (isDriveOpenLoop) {
+
+      // calculate the percent output from the currently demanded speed and the
+      // module's max speed. the currently demanded speed should never be above the
+      // module's max speed given that desiredState was properly desaturated
+      double percentOutput = state.speedMetersPerSecond / Constants.MAX_MODULE_SPEED;
+      driveMotor.set(ControlMode.PercentOutput, percentOutput);
+
+    } else {
+
+      // convert the wheel speed to Falcon velocity counts
+      double velocity = SN_Math.MPSToFalcon(
+          state.speedMetersPerSecond,
+          Constants.WHEEL_CIRCUMFERENCE,
+          Constants.DRIVE_GEAR_RATIO);
+
+      driveMotor.set(ControlMode.Velocity, velocity);
+    }
+
+    // convert angle to Falcon encoder counts
+    double angle = SN_Math.degreesToFalcon(
+        state.angle.getDegrees(),
+        Constants.STEER_GEAR_RATIO);
+
+    // if the module doesn't actually have any speed, don't bother steering it
+    if (Math.abs(
+        state.speedMetersPerSecond) < (prefDrivetrain.percentToSteer.getValue() * Constants.MAX_MODULE_SPEED)) {
+
+      angle = lastAngle;
+    }
+
+    steerMotor.set(ControlMode.Position, angle);
+
+    lastAngle = angle;
+  }
+
+  /**
+   * Neutral the drive motor output.
+   */
+  public void neutralDriveOutput() {
+    driveMotor.neutralOutput();
+  }
+
+  /**
+   * Reset the steer motor encoder to match the angle of the absolute encoder.
+   */
+  public void resetSteerMotorEncodersToAbsolute() {
+    double absoluteEncoderCount = SN_Math.degreesToFalcon(
+        getAbsoluteEncoder().getDegrees(),
+        Constants.STEER_GEAR_RATIO);
+
+    steerMotor.setSelectedSensorPosition(absoluteEncoderCount);
+  }
+
+  /**
+   * Get the absolute encoder with an offset applied. This angle will match the
+   * current angle of the module wheel.
+   * 
+   * @return Absolute encoder with offset applied
+   */
+  public Rotation2d getAbsoluteEncoder() {
+
+    double radians = Units.degreesToRadians(absoluteEncoder.getAbsolutePosition());
+
+    // This could make the value negative but it doesn't matter.
+    radians -= Units.degreesToRadians(absoluteEncoderOffset);
+
+    // There are a few reasons we're subtracting here instead of just applying an
+    // offset to the CANCoder.
+    // Firsty, this implmentation works with any absolute encoder. You don't have to
+    // rely on the encoder class on anything but getting a rotation value.
+    // Secondly, and this is very related to the first point, different encoder
+    // classes have identically named methods that are very different in
+    // functionality. Using the bare minimum from the encoder classes and doing the
+    // (very little) math ourselves minimizes opportunity for confusion
+
+    return Rotation2d.fromRadians(radians);
+  }
+
+  /**
+   * Reset the drive motor encoder count to 0.
+   */
+  public void resetDriveEncoderCount() {
+    driveMotor.setSelectedSensorPosition(0);
+  }
+
+  /**
+   * Get the current state of the swerve module. State includes a velocity and
+   * angle.
+   * 
+   * @return State of swerve module
+   */
+  public SwerveModuleState getState() {
+
+    double velocity = SN_Math.falconToMPS(
+        driveMotor.getSelectedSensorVelocity(),
+        Constants.WHEEL_CIRCUMFERENCE,
+        Constants.DRIVE_GEAR_RATIO);
+
+    Rotation2d angle = Rotation2d.fromDegrees(
+        SN_Math.falconToDegrees(
+            steerMotor.getSelectedSensorPosition(),
+            Constants.STEER_GEAR_RATIO));
+
+    return new SwerveModuleState(velocity, angle);
+  }
+
+  /**
+   * Get the current position of the swerve module. Position includes a distance
+   * and angle
+   * 
+   * @return Position of swerve module
+   */
+  public SwerveModulePosition getPosition() {
+
+    double distance = SN_Math.falconToMeters(
+        driveMotor.getSelectedSensorPosition(),
+        Constants.WHEEL_CIRCUMFERENCE,
+        Constants.DRIVE_GEAR_RATIO);
+
+    Rotation2d angle = Rotation2d.fromDegrees(
+        SN_Math.falconToDegrees(
+            steerMotor.getSelectedSensorPosition(),
+            Constants.STEER_GEAR_RATIO));
+
+    return new SwerveModulePosition(distance, angle);
+  }
+
+}
