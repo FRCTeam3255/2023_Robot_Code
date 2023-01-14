c3d3539a0dd6475381eaa115b83a7d8294d89456
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-14 13:03:53 -0800

    added drive command

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -4,10 +4,40 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.frcteam3255.joystick.SN_F310Gamepad;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotPreferences.prefDrivetrain;
+import frc.robot.subsystems.Drivetrain;
+
+/**
+ * Drive the robot.
+ * 
+ * Left stick controls the direction and speed of translation.
+ * Right trigger will slow the robot down as an analog input.
+ * 
+ * Right stick controls the rate of rotation.
+ *
+ */
 public class Drive extends CommandBase {
-  public Drive() {
+
+  Drivetrain subDrivetrain;
+  SN_F310Gamepad conDriver;
+
+  double xVelocity;
+  double yVelocity;
+  double rVelocity;
+  Pose2d velocity;
+
+  public Drive(Drivetrain subDrivetrain, SN_F310Gamepad conDriver) {
+
+    this.subDrivetrain = subDrivetrain;
+    this.conDriver = conDriver;
+
+    addRequirements(this.subDrivetrain);
   }
 
   @Override
@@ -16,10 +46,26 @@ public class Drive extends CommandBase {
 
   @Override
   public void execute() {
+
+    xVelocity = conDriver.getAxisLSX();
+    yVelocity = conDriver.getAxisLSY();
+    rVelocity = conDriver.getAxisRSX();
+
+    xVelocity *= Units.feetToMeters(prefDrivetrain.driveSpeed.getValue());
+    yVelocity *= Units.feetToMeters(prefDrivetrain.driveSpeed.getValue());
+    rVelocity *= Units.degreesToRadians(prefDrivetrain.turnSpeed.getValue());
+
+    velocity = new Pose2d(
+        xVelocity,
+        yVelocity,
+        new Rotation2d(rVelocity));
+
+    subDrivetrain.drive(velocity);
   }
 
   @Override
   public void end(boolean interrupted) {
+    subDrivetrain.neutralDriveOutputs();
   }
 
   @Override
