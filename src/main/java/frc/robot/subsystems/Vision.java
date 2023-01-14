ef0e4fc0feb9b5f7b2d4faf2766b7c7d293f55b3
ACat701 <90939494+ACat701@users.noreply.github.com>
2023-01-13 17:36:32 -0800

    Added photonPoseEstimator

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,10 +4,52 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.RobotPoseEstimator;
+import org.photonvision.RobotPoseEstimator.PoseStrategy;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+  RobotPoseEstimator photonPoseEstimator;
+
   public Vision() {
+    // TODO: Change to current april tag array upon new WPI update
+    AprilTagFieldLayout aprilTagFieldLayout = new AprilTagFieldLayout(null, 0, 0);
+
+    // TODO: get lifecam name, add more cameras when possible, add cameras to
+    // cameraList
+    PhotonCamera lifecam = new PhotonCamera("insertNameHere");
+    Transform3d robotToLifecam = new Transform3d(new Translation3d(0, 0, 0), new Rotation3d(0, 0, 0));
+
+    ArrayList<Pair<PhotonCamera, Transform3d>> cameraList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
+    cameraList.add(new Pair<PhotonCamera, Transform3d>(lifecam, robotToLifecam));
+    photonPoseEstimator = new RobotPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
+        cameraList);
+  }
+
+  // Returns the current estimated pose from vision & it's timestamp.
+  public Pair<Pose2d, Double> getPoseFromVision(Pose2d prevEstimatedRobotPose) {
+    photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+
+    double currentTime = Timer.getFPGATimestamp();
+    Optional<Pair<Pose3d, Double>> result = photonPoseEstimator.update();
+    if (result.isPresent()) {
+      return new Pair<Pose2d, Double>(result.get().getFirst().toPose2d(), currentTime - result.get().getSecond());
+    } else {
+      return new Pair<Pose2d, Double>(null, 0.0);
+    }
   }
 
   @Override
