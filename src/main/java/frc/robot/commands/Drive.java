700b183e2ea0d1d5cf04549b14a608b3f28f04c5
Ian Szalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-14 01:55:08 -0800

    added empty drive command

---
+++ src/main/java/frc/robot/commands/Drive.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Drive extends CommandBase {
+  public Drive() {
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
