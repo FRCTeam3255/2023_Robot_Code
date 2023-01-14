d6e7ed3faa1fdfea29d42f64937863d8ef3df123
Errorz <kane.barnes@gmail.com>
2023-01-14 15:35:40 -0800

    Leds to switchboard buttons
    Assigned switchboard buttons in place of controller buttons for Led control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@ import com.frcteam3255.components.SN_Blinkin;
 import com.frcteam3255.components.SN_Blinkin.PatternType;
 import com.frcteam3255.joystick.SN_DualActionStick;
 import com.frcteam3255.joystick.SN_F310Gamepad;
+import com.frcteam3255.joystick.SN_SwitchboardStick;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -15,11 +16,11 @@ import edu.wpi.first.wpilibj2.command.Commands;
 public class RobotContainer {
 
   // TODO: Update PWMChannel
-  
+
   private final SN_Blinkin leds = new SN_Blinkin(0);
 
   private final SN_F310Gamepad conDriver = new SN_F310Gamepad(0);
-  private final SN_DualActionStick conOperator = new SN_DualActionStick(1);
+  private final SN_SwitchboardStick conSwitchboard = new SN_SwitchboardStick(2);
 
   public RobotContainer() {
     configureBindings();
@@ -30,25 +31,15 @@ public class RobotContainer {
   // While held, Leds will change to given color, and turn off on release
   private void configureBindings() {
 
-    // Driver
-    conDriver.btn_X
+    // Switchboard
+    conSwitchboard.btn_1
         .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Violet)))
         .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
-    
-    conDriver.btn_Y
-      .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Yellow)))
-      .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
 
-
-
-    // Operator
-    conOperator.btn_X
-        .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Violet)))
+    conSwitchboard.btn_2
+        .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Yellow)))
         .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
-    
-    conOperator.btn_Y
-      .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Yellow)))
-      .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
+
   }
 
   public Command getAutonomousCommand() {
