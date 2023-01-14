22ed8a66428be0eac6dbc2736f54050835e9db5f
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-14 00:06:09 -0800

    added start of drivetrain subsystem

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,10 +4,33 @@
 
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.SN_SwerveModule;
 
 public class Drivetrain extends SubsystemBase {
+
+  private SN_SwerveModule[] modules;
+
+  private AHRS navX;
+
+  private SwerveDrivePoseEstimator poseEstimator;
+
   public Drivetrain() {
+
+    modules = new SN_SwerveModule[] {
+        new SN_SwerveModule(Constants.MODULE_0),
+        new SN_SwerveModule(Constants.MODULE_1),
+        new SN_SwerveModule(Constants.MODULE_2),
+        new SN_SwerveModule(Constants.MODULE_3)
+    };
+
+    navX = new AHRS();
+    navX.reset();
   }
 
   @Override
