717c6aa7f430c16f16f4de7d25f3d755287dee71
ACat701 <90939494+ACat701@users.noreply.github.com>
2023-01-14 16:15:16 -0800

    Added basic color sensor functionality

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,7 +1,13 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+
 public class RobotMap {
   public static final class mapDrivetrain {
 
+  }
+
+  public static final class mapIntake {
+    public static final I2C.Port COLOR_SENSOR_I2C = I2C.Port.kMXP;
   }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,15 +4,25 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.ColorSensorV3;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapIntake;
 
 public class Intake extends SubsystemBase {
 
+  ColorSensorV3 intakeColorSensor;
+
   public Intake() {
+    intakeColorSensor = new ColorSensorV3(mapIntake.COLOR_SENSOR_I2C);
   }
 
   @Override
   public void periodic() {
-
+    SmartDashboard.putString("Color Sensor Color", intakeColorSensor.getColor().toString());
+    SmartDashboard.putNumber("Color Sensor Red", intakeColorSensor.getRed());
+    SmartDashboard.putNumber("Color Sensor Green", intakeColorSensor.getGreen());
+    SmartDashboard.putNumber("Color Sensor Blue", intakeColorSensor.getBlue());
+    SmartDashboard.putNumber("Color Sensor Proximity", intakeColorSensor.getProximity());
   }
 }
