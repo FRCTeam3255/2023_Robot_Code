eb8bcd6ee10e5c2897f6aaa191422d2770ac528a
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-14 19:51:19 -0800

    SN_Preferences set to use network/hardcoded values from flag (#37)
    preferences nt/hc flag

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+import com.frcteam3255.preferences.SN_Preferences;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -17,6 +20,13 @@ public class Robot extends TimedRobot {
   // initialization code.
   @Override
   public void robotInit() {
+
+    if (RobotPreferences.useNetworkTables) {
+      SN_Preferences.usePreferences();
+    } else {
+      SN_Preferences.useDefaults();
+    }
+
     m_robotContainer = new RobotContainer();
   }
 
@@ -24,6 +34,8 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     // Runs the Scheduler. This is responsible for polling
     CommandScheduler.getInstance().run();
+
+    SmartDashboard.putBoolean("*NT Prefs*", RobotPreferences.useNetworkTables);
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
