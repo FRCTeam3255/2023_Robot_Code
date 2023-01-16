9d68f823254d4eef43b13681266cf4a92fb6adcb
Errorz <90939240+KaneBarnes@users.noreply.github.com>
2023-01-16 11:38:47 -0800

    Added led control (#30)
    * Added led control
    
    Added the code for led control testing
    
    * Tested leds to work
    
    Successfully tested Leds, and removed unused import
    
    * Leds to switchboard buttons
    
    Assigned switchboard buttons in place of controller buttons for Led control

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,18 +6,24 @@ package frc.robot;
 
 import com.frcteam3255.joystick.SN_F310Gamepad;
 
+import com.frcteam3255.components.SN_Blinkin;
+import com.frcteam3255.components.SN_Blinkin.PatternType;
+import com.frcteam3255.joystick.SN_SwitchboardStick;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 import frc.robot.RobotMap.mapControllers;
 import frc.robot.commands.Drive;
 import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.Commands;
 
 public class RobotContainer {
 
   private final Drivetrain subDrivetrain = new Drivetrain();
   private final Intake subIntake = new Intake();
-  
   private final SN_F310Gamepad conDriver = new SN_F310Gamepad(mapControllers.DRIVER);
+  private final SN_SwitchboardStick conSwitchboard = new SN_SwitchboardStick(mapControllers.SWITCHBOARD);
+  private final SN_Blinkin leds = new SN_Blinkin(mapControllers.BLINKIN);
 
   public RobotContainer() {
 
@@ -26,7 +32,24 @@ public class RobotContainer {
     configureBindings();
   }
 
+  // Leds
+
+  // While held, Leds will change to given color, and turn off on release
   private void configureBindings() {
+
+    // Switchboard
+
+    // Sets LED color to "violet" to indicate a purple game piece (cube) is being
+    // requested
+    conSwitchboard.btn_1
+        .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Violet)))
+        .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
+
+    // Sets LED color to "yellow" to indicate a yellow game piece (cone) is being
+    // requested
+    conSwitchboard.btn_2
+        .onTrue(Commands.runOnce(() -> leds.setPattern(PatternType.Yellow)))
+        .onFalse(Commands.runOnce(() -> leds.setPattern(PatternType.Black)));
   }
 
   public Command getAutonomousCommand() {
