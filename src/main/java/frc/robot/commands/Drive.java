08279a94faf46754cace909cba3a695631076401
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-19 16:35:48 -0800

    added some comments to the drive command

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -31,13 +31,19 @@ public class Drive extends CommandBase {
   Drivetrain subDrivetrain;
   SN_F310Gamepad conDriver;
 
+  // x and y velocity of robot in field relative coordinates
   double xVelocity;
   double yVelocity;
+
+  // rotational velocity of robot
   double rVelocity;
 
+  // translational velocity of robot (field relative)
   Translation2d translationVelocity;
+  // value to multiply with translational velocity
   double translationScalar;
 
+  // translational and rotational velocity of robot
   Pose2d velocity;
 
   public Drive(Drivetrain subDrivetrain, SN_F310Gamepad conDriver) {
@@ -55,23 +61,28 @@ public class Drive extends CommandBase {
   @Override
   public void execute() {
 
+    // assign the velocity values to joysticks. due to the differing coordinate
+    // systems between the robot and controller these don't line up
     xVelocity = -conDriver.getAxisLSY();
     yVelocity = conDriver.getAxisLSX();
     rVelocity = conDriver.getAxisRSX();
+
+    // value to multiply translationalVelocity with
     translationScalar = conDriver.getAxisRT();
 
+    // apply deadband to joystick inputs
     xVelocity = MathUtil.applyDeadband(xVelocity, constControllers.DRIVER_LEFT_STICK_Y_DEADBAND);
     yVelocity = MathUtil.applyDeadband(yVelocity, constControllers.DRIVER_LEFT_STICK_X_DEADBAND);
     rVelocity = MathUtil.applyDeadband(rVelocity, constControllers.DRIVER_RIGHT_STICK_X_DEADBAND);
     translationScalar = MathUtil.applyDeadband(translationScalar, constControllers.DRIVER_RIGHT_TRIGGER_DEADBAND);
 
+    // scale joystick inputs to be correct units
     xVelocity *= Units.feetToMeters(prefDrivetrain.driveSpeed.getValue());
     yVelocity *= Units.feetToMeters(prefDrivetrain.driveSpeed.getValue());
     rVelocity *= Units.degreesToRadians(prefDrivetrain.turnSpeed.getValue());
     translationScalar = SN_Math.interpolate(translationScalar, 0, 1, 1, prefDrivetrain.triggerValue.getValue());
 
-    translationVelocity = new Translation2d(xVelocity, yVelocity).times(translationScalar);
-
+    translationVelocity = new Translation2d(xVelocity, yVelocity);
     velocity = new Pose2d(
         translationVelocity,
         new Rotation2d(rVelocity));
