5aab3e90742acb9e3140516bc19818525a16bc19
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-21 15:21:37 -0800

    uncommented robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,13 +10,11 @@ import com.frcteam3255.components.SN_Blinkin;
 import com.frcteam3255.components.SN_Blinkin.PatternType;
 import com.frcteam3255.joystick.SN_SwitchboardStick;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Intake;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Vision;
-import frc.robot.Constants.constControllers;
 import frc.robot.RobotMap.mapControllers;
 import frc.robot.RobotPreferences.prefArm;
 import frc.robot.commands.AddVisionMeasurement;
@@ -24,14 +22,13 @@ import frc.robot.commands.Drive;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 
 public class RobotContainer {
 
-  // private final Drivetrain subDrivetrain = new Drivetrain();
-  // private final Intake subIntake = new Intake();
+  private final Drivetrain subDrivetrain = new Drivetrain();
+  private final Intake subIntake = new Intake();
   private final Arm subArm = new Arm();
-  // private final Vision subVision = new Vision();
+  private final Vision subVision = new Vision();
 
   private final SN_F310Gamepad conDriver = new SN_F310Gamepad(mapControllers.DRIVER_USB);
   private final SN_F310Gamepad conOperator = new SN_F310Gamepad(mapControllers.OPERATOR_USB);
@@ -40,15 +37,8 @@ public class RobotContainer {
 
   public RobotContainer() {
 
-    // subDrivetrain.setDefaultCommand(new Drive(subDrivetrain, conDriver));
-    // subVision.setDefaultCommand(new AddVisionMeasurement(subDrivetrain,
-    // subVision));
-
-    subArm.setDefaultCommand(new RunCommand(
-        () -> subArm.setJointPercentOutputs(
-            MathUtil.applyDeadband(conOperator.getAxisLSY(), constControllers.OPERATOR_LEFT_STICK_Y_DEADBAND),
-            MathUtil.applyDeadband(conOperator.getAxisRSY(), constControllers.OPERATOR_RIGHT_STICK_Y_DEADBAND)),
-        subArm));
+    subDrivetrain.setDefaultCommand(new Drive(subDrivetrain, conDriver));
+    subVision.setDefaultCommand(new AddVisionMeasurement(subDrivetrain, subVision));
 
     configureBindings();
   }
@@ -62,19 +52,18 @@ public class RobotContainer {
 
     // "reset gyro" for field relative but actually resets the orientation at a
     // higher level
-    // conDriver.btn_A
-    // .onTrue(Commands.runOnce(
-    // () -> subDrivetrain.resetPose(new
-    // Pose2d(subDrivetrain.getPose().getTranslation(), new Rotation2d(0)))));
+    conDriver.btn_A
+        .onTrue(Commands.runOnce(
+            () -> subDrivetrain.resetPose(new Pose2d(subDrivetrain.getPose().getTranslation(), new Rotation2d(0)))));
 
     // Operator
 
+    // Set the arm to a preset position (example bind, may not be necessary for comp
+    // bindings)
     conOperator.btn_A
-        .whileTrue(
-            Commands.runOnce(() -> subArm.setJointPositions(prefArm.shoulderP1, prefArm.elbowP1), subArm).repeatedly())
+        .whileTrue(Commands.runOnce(() -> subArm.setJointPositions(prefArm.shoulderPreset, prefArm.elbowPreset), subArm)
+            .repeatedly())
         .onFalse((Commands.runOnce(() -> subArm.setShoulderPercentOutput(0), subArm)));
-
-    conOperator.btn_X.onTrue(Commands.runOnce(() -> subArm.configure()));
 
     // Switchboard
 
