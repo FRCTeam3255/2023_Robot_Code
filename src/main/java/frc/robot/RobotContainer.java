2073e5c47a17ed037d8c9facc9d731f5b83b7c47
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-01-21 21:51:57 -0800

    added button to reset pose (debug)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -56,6 +56,9 @@ public class RobotContainer {
     conDriver.btn_A
         .onTrue(Commands.runOnce(
             () -> subDrivetrain.resetPose(new Pose2d(subDrivetrain.getPose().getTranslation(), new Rotation2d(0)))));
+    conDriver.btn_B
+        .onTrue(Commands.runOnce(
+            () -> subDrivetrain.resetPose(new Pose2d())));
 
     // Switchboard
 
