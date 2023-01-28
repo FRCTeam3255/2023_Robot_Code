bd4717d24f92cc911abdc1e922f9f91de49ca145
Alenguye582 <112772895+Alenguye582@users.noreply.github.com>
2023-01-28 09:35:45 -0800

    added through bore encoder to collector motor (#112)
    * added through bore encoder to collector motor
    
    * changed getAbsoluteEncoder to double instead of Rotation2d

--- src/main/java/frc/robot/subsystems/Collector.java
+++ src/main/java/frc/robot/subsystems/Collector.java
@@ -9,6 +9,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.frcteam3255.components.motors.SN_TalonFX;
 import com.frcteam3255.utils.SN_Math;
 
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -21,11 +24,15 @@ public class Collector extends SubsystemBase {
   SN_TalonFX pivotMotor;
   SN_TalonFX rollerMotor;
 
+  DutyCycleEncoder pivotEncoder;
+
   TalonFXConfiguration config;
 
   public Collector() {
     pivotMotor = new SN_TalonFX(mapCollector.PIVOT_MOTOR_CAN);
     rollerMotor = new SN_TalonFX(mapCollector.ROLLER_MOTOR_CAN);
+
+    pivotEncoder = new DutyCycleEncoder(mapCollector.PIVOT_ABSOLUTE_ENCODER_DIO);
 
     config = new TalonFXConfiguration();
     configure();
@@ -52,6 +59,7 @@ public class Collector extends SubsystemBase {
     config.slot0.closedLoopPeakOutput = prefCollector.collectorClosedLoopPeakOutput.getValue();
 
     pivotMotor.configAllSettings(config);
+    resetCollectortoAbsolute();
   }
 
   public void spinRollerMotor(double speed) {
@@ -68,11 +76,23 @@ public class Collector extends SubsystemBase {
     pivotMotor.set(ControlMode.Position, position);
   }
 
+  public double getCollectorAbsoluteEncoder() {
+    return pivotEncoder.getAbsolutePosition();
+  }
+
+  private void resetCollectortoAbsolute() {
+    double absoluteEncoderCount = SN_Math.degreesToFalcon(getCollectorAbsoluteEncoder(),
+        constCollector.GEAR_RATIO);
+    pivotMotor.setSelectedSensorPosition(absoluteEncoderCount);
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+ 
     if (Constants.OUTPUT_DEBUG_VALUES) {
       SmartDashboard.putNumber("Collector Pivot Motor Encoder", pivotMotor.getSelectedSensorPosition());
+      SmartDashboard.putNumber("Collector Pivot Absolute Encoder", getCollectorAbsoluteEncoder());
     }
   }
 }
