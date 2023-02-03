669f54ca1976f9af844fc38401e3f8a919a9fb1a
Errorz <kane.barnes@gmail.com>
2023-02-02 17:56:18 -0800

    Changed code to call  isGamePieceCollected and added code for merging
    Changed code to call  isGamePieceCollected and added commented code for when intake cube gets merged in to prevent merge conflicts

--- src/main/java/frc/robot/commands/intakeCone.java
+++ src/main/java/frc/robot/commands/intakeCone.java
@@ -7,17 +7,19 @@ package frc.robot.commands;
 import com.frcteam3255.components.SN_Blinkin;
 import com.frcteam3255.components.SN_Blinkin.PatternType;
 
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.RobotPreferences.prefArm;
 import frc.robot.RobotPreferences.prefCollector;
-import frc.robot.RobotPreferences.prefIntake;
+import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.Intake;
 
 public class intakeCone extends SequentialCommandGroup {
   Collector subCollector;
   Intake subIntake;
+  Arm subArm;
   SN_Blinkin leds;
 
   public intakeCone() {
@@ -27,14 +29,19 @@ public class intakeCone extends SequentialCommandGroup {
         new InstantCommand(() -> subCollector.setPivotMotorAngle(prefCollector.pivotAngleStartingConfig.getValue())),
 
         // Spin intake until a game piece is collected
-        new InstantCommand(() -> subIntake.setMotorSpeed(0)).until(subIntake::isGamePieceCollected),
+        new IntakeGamePiece(subIntake).until(subIntake::isGamePieceCollected).until(subIntake::isGamePieceCollected),
 
         // Stop motors
         new InstantCommand(() -> subIntake.setMotorSpeed(0)),
 
         // - raise arm to mid node position
+        // new InstantCommand(() ->
+        // subArm.setArmTipPositionInches(prefArm.armTipToMidPosX,
+        // prefArm.armTipToMidPosY)),
 
         // - LEDS: May want to be a constant since they can't be set as a preference
         new InstantCommand(() -> leds.setPattern(PatternType.Yellow)));
+    // new InstantCommand(() ->
+    // leds.setPattern(Constants.INTAKE_CONE_LED_PATTERN)));
   }
 }
