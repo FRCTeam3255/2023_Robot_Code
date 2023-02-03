1d2d2e3ff2f14cc8f22b91f6f30bced2925f954d
Errorz <kane.barnes@gmail.com>
2023-02-02 17:13:47 -0800

    Created intake cone command
    Created command, however a few steps need to be added before fully functional

---
+++ src/main/java/frc/robot/commands/intakeCone.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import com.frcteam3255.components.SN_Blinkin;
+import com.frcteam3255.components.SN_Blinkin.PatternType;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotPreferences.prefCollector;
+import frc.robot.RobotPreferences.prefIntake;
+import frc.robot.subsystems.Collector;
+import frc.robot.subsystems.Intake;
+
+public class intakeCone extends SequentialCommandGroup {
+  Collector subCollector;
+  Intake subIntake;
+  SN_Blinkin leds;
+
+  public intakeCone() {
+    addCommands(
+
+        // - Retract collector
+        new InstantCommand(() -> subCollector.setPivotMotorAngle(prefCollector.pivotAngleStartingConfig.getValue())),
+
+        // Spin intake until a game piece is collected
+        new InstantCommand(() -> subIntake.setMotorSpeed(0)).until(subIntake::isGamePieceCollected),
+
+        // Stop motors
+        new InstantCommand(() -> subIntake.setMotorSpeed(0)),
+
+        // - Move arm to intake/collection area
+
+        // - raise arm to mid node position -- Add something here to check if we have a
+        // cone before continuing
+
+        // - LEDS: May want to be a constant since they can't be set as a preference
+        new InstantCommand(() -> leds.setPattern(PatternType.Yellow)));
+  }
+}
