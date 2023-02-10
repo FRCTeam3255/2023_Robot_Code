1d59e7217859f9a0ec9ae896548dfebe2ed49daf
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-02-10 15:03:58 -0800

    added functionality to move arm command

--- src/main/java/frc/robot/commands/MoveArm.java
+++ src/main/java/frc/robot/commands/MoveArm.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotPreferences.prefCollector;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Collector;
 
@@ -12,9 +13,6 @@ public class MoveArm extends CommandBase {
 
   Arm subArm;
   Collector subCollector;
-
-  double goalX;
-  double goalY;
 
   public MoveArm(Arm subArm, Collector subCollector) {
     this.subArm = subArm;
@@ -29,10 +27,16 @@ public class MoveArm extends CommandBase {
 
   @Override
   public void execute() {
+
+    if (subCollector.getPivotMotorPosition() <= prefCollector.pivotAngleStartingConfig.getValue()) {
+      subArm.setArmTipPosition(subArm.getGoalArmTipPosition());
+    }
+
   }
 
   @Override
   public void end(boolean interrupted) {
+    subArm.neutralJointOutputs();
   }
 
   @Override
