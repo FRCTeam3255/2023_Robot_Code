5b3ff709e078f3f973c1e47422a7a65f045681fb
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-02-10 15:04:39 -0800

    removed safety logic for collector in move arm

--- src/main/java/frc/robot/commands/MoveArm.java
+++ src/main/java/frc/robot/commands/MoveArm.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotPreferences.prefCollector;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Collector;
 
@@ -28,9 +27,7 @@ public class MoveArm extends CommandBase {
   @Override
   public void execute() {
 
-    if (subCollector.getPivotMotorPosition() <= prefCollector.pivotAngleStartingConfig.getValue()) {
-      subArm.setArmTipPosition(subArm.getGoalArmTipPosition());
-    }
+    subArm.setArmTipPosition(subArm.getGoalArmTipPosition());
 
   }
 
