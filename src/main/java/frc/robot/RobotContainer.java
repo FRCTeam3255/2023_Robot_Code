3f264e66d9e2fe79ff71b51e2c84bbdcbab52e9d
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-02-11 18:49:05 -0800

    add intake cone

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,6 +101,7 @@ public class RobotContainer {
     conOperator.btn_LBump.onTrue(new intakeCube(subArm, subCollector, subIntake, leds));
 
     // TODO: Run IntakeCone command (btn_RB)
+    conOperator.btn_RBump.whileTrue(new IntakeCone(subCollector, subIntake, subArm, leds));
     // TODO: Run PrepPlace command (btn_LT)
     // TODO: Run PlaceGamePiece command (btn_RT)
 
