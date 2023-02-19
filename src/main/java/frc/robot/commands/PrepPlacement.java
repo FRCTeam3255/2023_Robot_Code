a94dda2b6dd264ec40c253a4fa36489e2b107833
ACat701 <90939494+ACat701@users.noreply.github.com>
2023-02-18 17:34:44 -0800

    Made PrepPlacement an InstantCommand

--- src/main/java/frc/robot/commands/PrepPlacement.java
+++ src/main/java/frc/robot/commands/PrepPlacement.java
@@ -4,33 +4,77 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.ColorSensorV3;
+import com.frcteam3255.preferences.SN_DoublePreference;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.constControllers.ScoringColumn;
+import frc.robot.Constants.constControllers.ScoringLevel;
+import frc.robot.Constants.constVision.GamePiece;
+import frc.robot.RobotPreferences.prefArm;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 
-public class PrepPlacement extends SequentialCommandGroup {
+public class PrepPlacement extends InstantCommand {
+  Arm subArm;
+  Drivetrain subDrivetrain;
+  Intake subIntake;
+  GamePiece currentGamePiece;
+  ScoringColumn scoringColumn;
+  ScoringLevel scoringLevel;
 
-  // TODO: Make sure only using Y axis
-  Arm arm;
-  ColorSensorV3 colorSensor;
-  Drivetrain drivetrain;
-  Intake intake;
+  boolean isCubeColumn;
+  SN_DoublePreference shoulderDegrees;
+  SN_DoublePreference elbowDegrees;
 
-  public PrepPlacement(Arm subArm, ColorSensorV3 colorSensor, Drivetrain subDrivetrain, Intake subIntake) {
-    addCommands(
-        // Get game piece type via color sensor
-        new InstantCommand(() -> subIntake.getGamePieceType())
+  public PrepPlacement(Arm subArm, Drivetrain subDrivetrain, Intake subIntake,
+      ScoringColumn scoringColumn, ScoringLevel scoringLevel) {
+    this.subArm = subArm;
+    this.subDrivetrain = subDrivetrain;
+    this.subIntake = subIntake;
+    this.scoringColumn = scoringColumn;
+    this.scoringLevel = scoringLevel;
 
-    // Get operator input via num pad
+    addRequirements(subArm);
+  }
 
-    // If position is valid, drive and move the arm on the Y-axis
+  @Override
+  public void initialize() {
+    currentGamePiece = subIntake.getGamePieceType();
+    isCubeColumn = (scoringColumn == ScoringColumn.SECOND || scoringColumn == ScoringColumn.FIFTH
+        || scoringColumn == ScoringColumn.EIGHTH);
 
-    // TODO: Make sure to assign button binding for driver
+    if (isCubeColumn && currentGamePiece != GamePiece.CONE) {
+      // Node is good, do driving code
+    } else {
+      if (scoringLevel == ScoringLevel.HYBRID) {
+        // Node is actually good sike
+      } else {
+        // Node is bad, do something else ig
+        // TODO: Decide what to do if the node is invalid
+      }
+    }
 
-    );
+    switch (scoringLevel) {
+      case HYBRID:
+        shoulderDegrees = prefArm.armPresetLowElbowAngle;
+        elbowDegrees = prefArm.armPresetLowShoulderAngle;
+        break;
+      case MID:
+        shoulderDegrees = prefArm.armPresetMidElbowAngle;
+        elbowDegrees = prefArm.armPresetMidShoulderAngle;
+        break;
+      case HIGH:
+        shoulderDegrees = prefArm.armPresetHighElbowAngle;
+        elbowDegrees = prefArm.armPresetHighShoulderAngle;
+        break;
+      case NONE:
+        // TODO: WHAT DO?!??!??!?
+    }
+    // If position is valid, drive
+    // TODO: Add driving code
+
+    // and move the arm
+    subArm.setGoalAngles(shoulderDegrees, elbowDegrees);
   }
 }
