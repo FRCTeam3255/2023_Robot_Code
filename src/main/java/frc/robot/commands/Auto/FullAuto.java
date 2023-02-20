fc920c616c0e18870a5916384e965f15f525aa40
Alenguye582 <90939240+KaneBarnes@users.noreply.github.com>
2023-02-20 13:58:38 -0800

    Added placeGamePiece and Dock logic
    Updated auto from qa import

--- src/main/java/frc/robot/commands/Auto/FullAuto.java
+++ src/main/java/frc/robot/commands/Auto/FullAuto.java
@@ -11,8 +11,10 @@ import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
+import frc.robot.commands.Dock;
 import frc.robot.commands.IntakeCone;
 import frc.robot.commands.MoveArm;
+import frc.robot.commands.PlaceGamePiece;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -24,15 +26,10 @@ public class FullAuto extends SequentialCommandGroup {
   Drivetrain drivetrain;
   Intake intake;
 
+  PlaceGamePiece placeGamePiece;
   IntakeCone intakeCone;
   MoveArm moveArm;
-
-  // TODO:
-  // [ ] Place cone
-  // [X] Drive/Rotate (auto path)
-  // [X] Intake cone
-  // [X] Drive to charge station (auto path)
-  // [ ] Dock/Engage - Requires gyro logic
+  Dock dock;
 
   public FullAuto(Arm subArm, Collector subCollector, Drivetrain subDrivetrain, Intake subIntake,
       IntakeCone intakeCone, MoveArm moveArm) {
@@ -42,11 +39,11 @@ public class FullAuto extends SequentialCommandGroup {
     drivetrain = subDrivetrain;
     intake = subIntake;
 
+    placeGamePiece = new PlaceGamePiece(subArm, subCollector, subIntake, null, null);
     intakeCone = new IntakeCone(subCollector, subIntake, subArm);
 
     addCommands(
-        // TODO: Change this to placeCone once command is created
-        intakeCone,
+        placeGamePiece,
 
         Commands.parallel(
             // TODO: drivetrain.twoConePath should be the updated FullAuto path
@@ -54,6 +51,8 @@ public class FullAuto extends SequentialCommandGroup {
                 .andThen(new InstantCommand(() -> drivetrain.neutralDriveOutputs(), drivetrain)),
 
             // Delay to make sure intake doesn't collide after placing cone
-            intakeCone.withTimeout(2)));
+            intakeCone.withTimeout(2)),
+
+        dock);
   }
 }
