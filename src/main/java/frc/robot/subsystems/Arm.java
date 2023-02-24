7415fb024c0dbaaa90c1cac13c97ae2bfcf11076
Ian Szalai <60115484+IanSzalai@users.noreply.github.com>
2023-02-24 02:26:20 -0800

    reset motor encoders to abs enc after delay

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -57,6 +58,8 @@ public class Arm extends SubsystemBase {
     goalElbowAngle = new Rotation2d();
 
     configure();
+    Timer.delay(2.5);
+    resetJointEncodersToAbsolute();
   }
 
   public void configure() {
