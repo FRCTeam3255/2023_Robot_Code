4a85fee0b79a9ab3542fb14e3ad0c7009f5b7230
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-02-27 13:23:12 -0800

    limit snapping to when the shoulder is at -90

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,6 +53,7 @@ public class RobotContainer {
     subDrivetrain
         .setDefaultCommand(new Drive(
             subDrivetrain,
+            subArm,
             conDriver.axis_LeftY,
             conDriver.axis_LeftX,
             conDriver.axis_RightX,
