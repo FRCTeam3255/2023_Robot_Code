ca48079fdfdec28f299d9f7e5941b6e88220c937
IanSzalai <60115484+IanSzalai@users.noreply.github.com>
2023-03-22 14:14:42 -0700

    added intake cube command (it worked)

---
+++ src/main/java/frc/robot/commands/IntakeCube.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.constArm.ArmState;
+import frc.robot.RobotPreferences.prefCollector;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Collector;
+import frc.robot.subsystems.Intake;
+
+public class IntakeCube extends SequentialCommandGroup {
+
+  Arm subArm;
+  Collector subCollector;
+  Intake subIntake;
+
+  public IntakeCube(Arm subArm, Collector subCollector, Intake subIntake) {
+
+    this.subArm = subArm;
+    this.subCollector = subCollector;
+    this.subIntake = subIntake;
+
+    addCommands(
+
+        subArm.stateFromStowCommand(ArmState.COLLECTOR_MOVING),
+        Commands.waitUntil(() -> subArm.isCurrentState(ArmState.COLLECTOR_MOVING)),
+
+        new PivotCollector(subCollector, subArm, prefCollector.pivotAngleCollecting)
+            .until(() -> subCollector.isAngleCollecting()),
+
+        subArm.stateFromStowCommand(ArmState.COLLECTOR_COLLECTING_TRANSITION),
+        Commands.waitUntil(() -> subArm.isCurrentState(ArmState.COLLECTOR_COLLECTING_TRANSITION)),
+
+        Commands.runOnce(() -> subArm.setGoalState(ArmState.COLLECTOR_COLLECTING)),
+        Commands.waitUntil(() -> subArm.isCurrentState(ArmState.COLLECTOR_COLLECTING)),
+
+        new IntakeGamePiece(subIntake).until(() -> subIntake.isGamePieceCollected()),
+
+        Commands.runOnce(() -> subArm.setGoalState(ArmState.COLLECTOR_COLLECTING_TRANSITION)),
+        Commands.waitUntil(() -> subArm.isCurrentState(ArmState.COLLECTOR_COLLECTING_TRANSITION)),
+
+        subArm.stateFromStowCommand(ArmState.COLLECTOR_MOVING),
+        Commands.waitUntil(() -> subArm.isCurrentState(ArmState.COLLECTOR_MOVING)),
+
+        new PivotCollector(subCollector, subArm, prefCollector.pivotAngleStowed),
+        Commands.waitUntil(() -> subCollector.isStowed())
+
+    );
+  }
+}
